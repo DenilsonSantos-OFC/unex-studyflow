b644a3e03e6d308d396887c0203ea80bc673a311
Denilson Santos <144867249+DenilsonSantos-OFC@users.noreply.github.com>
2024-11-12 00:03:03 -0300

    (Back-End) Alteração do retorno do token pós-autenticação
    Inclui atribuição do token diretamente no corpo ao invés de ser atrelado no cabeçalho, junto com a adição de uma mensagem de retorno no cadastro bem-sucedido.

--- back-end/src/controllers/usuario-controller.js
+++ back-end/src/controllers/usuario-controller.js
@@ -33,8 +33,8 @@ class UsuarioController {
         }
         if (!idDoUsuario)
             return respostaHTTP.enviarProibicao(msgFalha)
-        respostaHTTP.registrarToken(UsuarioServices.gerarToken(idDoUsuario))
-        return respostaHTTP.enviarOk(msgOk)
+        respostaHTTP.registrarToken()
+        return respostaHTTP.enviarOk(msgOk, {token: UsuarioServices.gerarToken(idDoUsuario)})
     }
 
     static async consultar(req, res) {
@@ -67,7 +67,7 @@ class UsuarioController {
         }
         if (!usuarioFoiCadastrado)
             return respostaHTTP.enviarErroInterno(msgFalha)
-        return respostaHTTP.enviarOkPraCadastro()
+        return respostaHTTP.enviarOkPraCadastro('Usuário cadastrado com sucesso.')
     }
 
     static async alterar(req, res) {
@@ -87,97 +87,4 @@ class UsuarioController {
         return respostaHTTP.enviarOk(msgOk)
     }
 
-
-
-
-
-
-    // -------------------------------------------------------------------------------------------------------- //
-
-    /**
-     * @method autenticar
-     * @static @async
-     * @description
-     * Lida com requisições de autenticação.
-     * É responsável por chamar o recurso interno responsável por realizar esta operação no banco de dados.
-     * @param {Request} req - Objeto de requisição, contendo os dados necessários.
-     *    @param {string} req.body.email - Requer campo "email" especificado na requisição.
-     *    @param {string} req.body.senha - Requer campo "senha" especificada na requisição.
-     * @param {Response} res - Objeto da resposta, que será manipulada pelo controlador.
-     * @returns {Object}
-     * Um objeto criado a partir do resultado da operação, moldado pela classe RespostaHTTP.
-     * Ela contém algumas informações...
-     * - Código HTTP: A resposta pode ser enviada com um dos seguintes códigos HTTP abaixo...
-     *   - 200 (se a autenticação for realizada com sucesso);
-     *   - 403 (se as credenciais passadas na requisição forem inválidas);
-     *   - 500 (se ocorrer um erro de processamento por parte do servidor).
-     * - Mensagem: É única para cada código HTTP e reflete diretamente o resultado da operação.
-     *   - 200: Usuário autenticado com sucesso.
-     *   - 403: Credenciais inválidas.
-     *   - 500: **Mensagem definidda por padrão pela aplicação**
-     * - Autorização: Se a autenticação for realizada com sucesso, o objeto conterá um token registrado no cabeçalho.
-     *   (Este token contém o id do usuário de forma cifrada, o que permitirá a identificação do próprio em requisições posteriores.)
-    */
-
-
-    /**
-     * @method consultar
-     * @static @async
-     * @description
-     * Lida com requisições de consulta.
-     * É responsável por chamar o recurso interno responsável por realizar esta operação no banco de dados.
-     * Só é possível o usuário consultar suas próprias informações.
-     * @param {Request} req - Objeto de requisição, contendo os dados necessários.
-     *    @param {string} req.headers.Authorization - Requer campo "Authorization" para autenticação e identificação do usuário.
-     * @param {Response} res - Objeto da resposta, que será manipulada pelo controlador.
-     * @returns {Object}
-     * Um objeto criado a partir do resultado da operação, moldado pela classe RespostaHTTP.
-     * - Código HTTP: Pode retornar...
-     *   - 300
-     *  200 (se OK), 404 (se usuário não encontrado) ou 500 (se erro no servidor).
-     *                O erro 404 não deveria ocorrer em situações normais, já que o ID do usuário é obtido a partir do token,
-     *                que é fornecido pelo próprio servidor. Caso esse erro ocorra, a situação deve ser estudada.
-     * - Mensagem: Retornará uma mensagem sobre o resultado da operação.
-     */
-
-
-    /**
-     * @method cadastrar
-     * @static @async
-     * @description
-     * Lida com requisições de cadastro.
-     * É responsável por chamar o recurso interno responsável por realizar esta operação no banco de dados.
-     * @param {Request} req - Objeto de requisição, contendo os dados necessários.
-     *    @param {string} req.body.nome - Requer campo "nome" especificado na requisição.
-     *    @param {string} req.body.email - Requer campo "email" especificado na requisição (não pode ser um já registrado no banco de dados).
-     *    @param {string} req.body.senha - Requer campo "senha" especificada na requisição.
-     * @param {Response} res - Objeto da resposta, que será manipulada pelo controlador.
-     * @returns {Object}
-     * Um objeto criado a partir do resultado da operação, moldado pela classe RespostaHTTP.
-     * - Código HTTP: Pode retornar 201 (se Created) ou 500 (se qualquer erro acontecer).
-     * - Mensagem: Retornará uma mensagem sobre o resultado da operação.
-     */
-
-
-    /**
-     * @method alterar
-     * @static @async
-     * @description
-     * Lida com requisições de alteração de dados.
-     * É responsável por chamar o recurso interno responsável por realizar esta operação no banco de dados.
-     * Vários campos podem ser passados de forma opcional para alteração, mas não funciona com requisições sem campo nenhum. 
-     * @param {Request} req - Objeto de requisição, contendo os dados que serão alterados.
-     *    @param {string} req.body.nome - Pode passar o campo "nome" na requisição (opcional).
-     *    @param {string} req.body.genero - Pode passar o campo "genero" na requisição (opcional).
-     *    @param {string} req.body.email - Pode passar o campo "email" na requisição (opcional).
-     *    @param {string} req.body.senha - Pode passar o campo "senha" na requisição (opcional).
-     *    @param {string} req.body.dataDeNascimento - Pode passar o campo "dataDeNascimento" na requisição (opcional).
-     * @param {Response} res - Objeto da resposta, que será manipulada pelo controlador.
-     * @returns {Object}
-     * Um objeto criado a partir do resultado da operação, moldado pela classe RespostaHTTP.
-     * - Código HTTP: Pode retornar 200 (se OK) ou 500 (se qualquer erro acontecer).
-     * - Mensagem: Retornará uma mensagem sobre o resultado da operação.
-     */
-
-    
 } module.exports = UsuarioController
